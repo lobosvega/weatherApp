e87ed4e2b0b89b4383d95b8dcc8e1d2c298e61ef
J Lobos Vega <jalv.vg@gmail.com>
2019-07-25 19:12:23 -0400

    componets
    methods and componets finished, set states and props.

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -22,15 +22,40 @@ class App extends React.Component {
         const country = e.target.elements.country.value;
         const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}&units=metric`);
         const data = await api_call.json();
-        console.log(data);
-        
+        if (city && country) {
+            console.log(data);
+            this.setState({
+                temperature: data.main.temp,
+                city: data.name,
+                country: data.sys.country,
+                humidity: data.main.humidity,
+                description: data.weather[0].description,
+                error:""
+            });
+        } else {
+            this.setState({
+                temperature: undefined,
+                city: undefined,
+                country: undefined,
+                humidity: undefined,
+                description: undefined,
+                error:"please enter value"
+            });
+        }
     }
     render() {
         return (    
         <div>
             <Titles />
-            <Form getWeather={this.getWeather}/>
-            <Weather />
+            <Form getWeather={this.getWeather} />
+            <Weather 
+                temperature={this.state.temperature}
+                city={this.state.city}
+                country={this.state.country}
+                humidity={this.state.humidity}
+                description={this.state.description}
+                error={this.state.error}
+                />
         </div>
     );
   }
