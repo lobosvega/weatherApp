5342b13014b0431344ca72f805e371693912861f
J Lobos Vega <jalv.vg@gmail.com>
2019-07-25 20:27:08 -0400

    form and titles js undated

--- weather-app/src/components/Form.js
+++ weather-app/src/components/Form.js
@@ -1,15 +1,12 @@
 import React from "react";
 
-class Form extends React.Component {
-	render() {
-		return (
-			<form onSubmit={this.props.getWeather}>
-				<input type="text" name="city" placeholder="city"/>
-				<input type="text" name="country" placeholder="country"/>
-				<button>Current Weather</button>
-			</form>
-		);
-	}
-};
+const Form = props => (
+	<form onSubmit={props.getWeather}>
+		<input type="text" name="city" placeholder="city"/>
+		<input type="text" name="country" placeholder="country"/>
+		<button>Current Weather</button>
+	</form>
+	
+);
 
 export default Form;
