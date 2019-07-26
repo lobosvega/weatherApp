83a5abbd588cf806c0bcef6b4d1ad084eb9ebece
J Lobos Vega <jalv.vg@gmail.com>
2019-07-25 21:28:02 -0400

    styles applied

--- weather-app/src/components/Titles.js
+++ weather-app/src/components/Titles.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const  Titles = () => (
 	<div>
-		<h1>Weather Report</h1>
-		<h3> Find out temperature, conditions and more...</h3>
+		<h1 className="title-container__title">Weather Report</h1>
+		<h3 className="title-container__subtitle"> Find out temperature, conditions and more...</h3>
 	</div>
 	
 );
